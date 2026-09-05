e680bd243cd2d88bb796b7b05706a82f31c2cb4d
thefool666 <123851710+thefool666@users.noreply.github.com>
2026-09-06 01:23:59 +0800

    更新 AdbHelper.java

--- app/src/main/java/cd/fool/adbport/AdbHelper.java
+++ app/src/main/java/cd/fool/adbport/AdbHelper.java
@@ -10,6 +10,7 @@ import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.X509v3CertificateBuilder;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.conscrypt.Conscrypt;
