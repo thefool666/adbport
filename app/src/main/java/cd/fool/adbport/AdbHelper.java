c1a923e9137cdaf996a9827db41263704818d450
thefool666 <123851710+thefool666@users.noreply.github.com>
2026-09-06 01:12:17 +0800

    更新 AdbHelper.java

--- app/src/main/java/cd/fool/adbport/AdbHelper.java
+++ app/src/main/java/cd/fool/adbport/AdbHelper.java
@@ -9,7 +9,7 @@ import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.X509v3CertificateBuilder;
-org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
 import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.conscrypt.Conscrypt;
